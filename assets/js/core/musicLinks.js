c4d40c580771f264e5c3a8178dc1531ffe5fc569
therealunrealstory <therealunrealstory@gmail.com>
2025-09-04 14:51:19 +0300

    1451

--- assets/js/core/musicLinks.js
+++ assets/js/core/musicLinks.js
@@ -1,14 +1,14 @@
 // /assets/js/core/musicLinks.js
 export const MUSIC_LINKS = {
-  EN: 'https://ditto.com/trus-en',
+  EN: 'https://ditto.fm/our-real-and-unreal-story',
   ES: 'https://ditto.fm/nuestra-historia-real-e-irreal',
-  FR: 'https://ditto.com/trus-fr',
-  IT: 'https://ditto.com/trus-it',
-  DE: 'https://ditto.com/trus-de',
+  FR: 'https://music.youtube.com/watch?v=gMWVqQcTbPg&list=OLAK5uy_nBAIdKx4LXM5pNfv9ur8p-OXtblsneFsE',
+  IT: 'https://ditto.fm/la-nostra-storia-reale-e-irreale',
+  DE: 'https://ditto.fm/unsere-reale-und-irreale-geschichte',
   PT: 'https://ditto.fm/nossa-historia-real-e-irreal',
-  RU: 'https://ditto.com/trus-ru',
-  CN: 'https://ditto.com/trus-cn',
-  AR: 'https://ditto.com/trus-ar',
+  RU: 'https://ditto.fm/nasa-realno-nerealnaia-istoriia',
+  CN: 'https://ditto.fm/release-4565891',
+  AR: 'https://ditto.fm/kstna-alhkyky-oallamaakol',
 };
 
 export function getMusicLink(locale) {
