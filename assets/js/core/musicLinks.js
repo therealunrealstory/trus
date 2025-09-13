2594ff3fbae668628243ad5d4dbe934dbb9d6f95
therealunrealstory <therealunrealstory@gmail.com>
2025-09-13 04:57:52 +0300

    Update musicLinks.js

--- assets/js/core/musicLinks.js
+++ assets/js/core/musicLinks.js
@@ -2,7 +2,7 @@
 export const MUSIC_LINKS = {
   EN: 'https://ditto.fm/our-real-and-unreal-story',
   ES: 'https://ditto.fm/nuestra-historia-real-e-irreal',
-  FR: 'https://music.youtube.com/watch?v=gMWVqQcTbPg&list=OLAK5uy_nBAIdKx4LXM5pNfv9ur8p-OXtblsneFsE',
+  FR: 'https://ditto.fm/nico_trus-notre_histoire_reelle_et_irreelle',
   IT: 'https://ditto.fm/la-nostra-storia-reale-e-irreale',
   DE: 'https://ditto.fm/unsere-reale-und-irreale-geschichte',
   PT: 'https://ditto.fm/nossa-historia-real-e-irreal',
