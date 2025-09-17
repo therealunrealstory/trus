505e0e852248f8e0d4e176e6f6704acc6ade9e52
therealunrealstory <therealunrealstory@gmail.com>
2025-09-17 03:31:02 +0300

    0330

--- assets/js/core/boot.js
+++ assets/js/core/boot.js
@@ -84,7 +84,7 @@ function onceFlag(el, flag) {
     e.preventDefault();
     const shareData = {
       title: t('share.title', 'The Real Unreal Story'),
-      text:  t('share.text',  'Support Adam’s journey'),
+	  text: t('share.text', 'Read and share the story'),
       url:   location.href
     };
     try {
